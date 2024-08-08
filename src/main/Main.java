3b59dab46d57a297b5f3209b0a9d878eb4e48f44
Guilherme Monteiro <guilhermevmonteiro2512@gmail.com>
2024-08-08 17:15:26 -0300

    Adiciona os maiores salarios

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,59 +1,65 @@
 package main;
 
+import java.util.List;
+
 import javax.swing.JOptionPane;
 
 import main.features.salariosMaiores.SalariosMaiores;
+import main.utils.CargaDados;
+import main.utils.FormatadorDeLista;
+import main.utils.Funcionario;
 import main.utils.view.EntradaSaida;
 
 public class Main {
 
 	public static void main(String[] args) {
 
+		List<Funcionario> dados = CargaDados.getListaFuncionarios();
+
 		int opcao;
 
 		do {
 
 			opcao = EntradaSaida.coletaOpcaoMenu();
-			
-			
 
 			switch (opcao) {
-				case 1: {
-					JOptionPane.showMessageDialog(null, SalariosMaiores.listaMaioresSalarios(null));
-					break;
-				}
-				case 2: {
-	
-					break;
-				}
-				case 3: {
-					
-					break;
-				}
-				case 4: {
-	
-					break;
-				}
-				case 5: {
-	
-					break;
-				}
-				case 6: {
-	
-					break;
-				}
-				case 7: {
-	
-					break;
-				}
-				case 8: {
-	
-					break;
-				}
-	
-				default: {
-					JOptionPane.showMessageDialog(null, "Opcão inválida, tente novamente!");
-				}
+			case 1: {
+				JOptionPane.showMessageDialog(null,
+						FormatadorDeLista.formatarListaDeFuncionarios(SalariosMaiores.listaMaioresSalarios(dados)));
+				break;
+			}
+			case 2: {
+
+				break;
+			}
+			case 3: {
+				
+				break;
+			}
+			case 4: {
+
+				break;
+			}
+			case 5: {
+
+				break;
+			}
+			case 6: {
+
+				break;
+			}
+			case 7: {
+
+				break;
+			}
+			case 8: {
+
+				break;
+			}
+
+			default: {
+				JOptionPane.showMessageDialog(null, "Opcão inválida, tente novamente!");
+			}
 			}
 
 		} while (opcao != 8);
---
+++ src/main/utils/FormatadorDeLista.java
@@ -0,0 +1,24 @@
+package main.utils;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FormatadorDeLista {
+
+	public static String formatarListaDeFuncionarios(List<Funcionario> funcionarios) {
+
+		List<String> funcionariosString = funcionarios.stream()
+				.map(funcionario -> "Funcionario: " + funcionario.getNome() + " - Departamento: "
+						+ funcionario.getCargo() + " - Salário: R$" + String.format("%.2f", funcionario.getSalario())
+						+ "\n")
+				.collect(Collectors.toList());
+
+		StringBuilder builder = new StringBuilder();
+
+		for (String string : funcionariosString) {
+			builder.append(string);
+		}
+
+		return builder.toString();
+	}
+}
