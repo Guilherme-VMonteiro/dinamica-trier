435df633691b983102946b1242fd672df8cebaf0
Patrick Zanela Medeiros <patrickzm290493@gmail.com>
2024-08-08 21:17:54 -0300

    testemedia

--- src/main/features/maiormedia/MaiorMedia.java
+++ src/main/features/maiormedia/MaiorMedia.java
@@ -1,27 +1,36 @@
 package main.features.maiormedia;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.List;
 import java.util.Map;
 
 import main.utils.DepartamentoEnum;
+import main.utils.Funcionario;
 
 public class MaiorMedia {
 
+    public static DepartamentoEnum encontrarDepartamentoComMaiorMedia(Map<DepartamentoEnum, List<Funcionario>> mapaDeFuncionarios) {
+        DepartamentoEnum departamentoComMaiorMedia = null;
+        BigDecimal maiorMedia = BigDecimal.ZERO;
 
-	    public DepartamentoEnum encontrarDepartamentoComMaiorMedia(Map<DepartamentoEnum, BigDecimal> mapaMedias) {
-	        DepartamentoEnum departamentoComMaiorMedia = null;
-	        BigDecimal maiorMedia = BigDecimal.ZERO;
+        for (Map.Entry<DepartamentoEnum, List<Funcionario>> entry : mapaDeFuncionarios.entrySet()) {
+            DepartamentoEnum departamento = entry.getKey();
+            List<Funcionario> funcionarios = entry.getValue();
 
-	        for (Map.Entry<DepartamentoEnum, BigDecimal> entry : mapaMedias.entrySet()) {
-	            if (entry.getValue().compareTo(maiorMedia) > 0) {
-	                maiorMedia = entry.getValue();
-	                departamentoComMaiorMedia = entry.getKey();
-	            }
-	        }
+            BigDecimal somaSalarios = BigDecimal.ZERO;
+            for (Funcionario funcionario : funcionarios) {
+                somaSalarios = somaSalarios.add(funcionario.getSalario());
+            }
 
-	        return departamentoComMaiorMedia;
-	    }
-	
+            BigDecimal mediaSalarial = somaSalarios.divide(new BigDecimal(funcionarios.size()), RoundingMode.HALF_UP);
 
+            if (mediaSalarial.compareTo(maiorMedia) > 0) {
+                maiorMedia = mediaSalarial;
+                departamentoComMaiorMedia = departamento;
+            }
+        }
 
+        return departamentoComMaiorMedia;
+    }
 }
---
+++ src/main/features/maiormedia/MaiorMediaTest.java
@@ -0,0 +1,42 @@
+package main.features.maiormedia;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import main.utils.DepartamentoEnum;
+import main.utils.Funcionario;
+
+class MaiorMediaTest {
+
+	@DisplayName("Deve retornar o valor do departamento com o maior media salarial")
+	@Test
+	public void testeMaiorMediaSalarial() {
+		
+		Funcionario func1 = new Funcionario("João Silva", DepartamentoEnum.ADMINISTRACAO, new BigDecimal("9000"));
+		Funcionario func2 = new Funcionario("Maria Oliveira", DepartamentoEnum.RH, new BigDecimal("8000"));
+		Funcionario func3 = new Funcionario("Pedro Souza", DepartamentoEnum.DESENVOLVIMENTO, new BigDecimal("7000"));
+		Funcionario func4 = new Funcionario("Carlos Alves", DepartamentoEnum.ADMINISTRACAO, new BigDecimal("9200"));
+
+		Map<DepartamentoEnum, List<Funcionario>> mapaDeFuncionarios = new HashMap<>();
+		
+		mapaDeFuncionarios.put(DepartamentoEnum.ADMINISTRACAO, new ArrayList<>(Arrays.asList(func1, func4)));
+		mapaDeFuncionarios.put(DepartamentoEnum.RH, new ArrayList<>(Arrays.asList(func2)));
+		mapaDeFuncionarios.put(DepartamentoEnum.DESENVOLVIMENTO, new ArrayList<>(Arrays.asList(func3)));
+
+		DepartamentoEnum resultado = MaiorMedia.encontrarDepartamentoComMaiorMedia(mapaDeFuncionarios);
+		
+		Assertions.assertEquals(DepartamentoEnum.ADMINISTRACAO, resultado);
+		
+	}
+
+}
